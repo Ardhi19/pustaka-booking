8206b388b447ce601b883595ccd43a1d1f711fea
Ardhi19 <ardhi.rachmadi@gmail.com>
2022-03-27 22:36:41 +0700

    contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan<h1>";
+    echo " Nama saya Ardhi rachmadi
+        saya tinggal di daerah legoso olah raga yang saya sukai
+        adalah sepakbola dan bulu tangkis";
+    }
+}
